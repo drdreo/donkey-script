2851f327617fd78e95c21806138915b2039dc27c
DrDreo <code@anderas-hahn.at>
2024-01-28 17:33:09 +0100

    cleanup

--- src/donkey/constants/constants.go
+++ src/donkey/constants/constants.go
@@ -2,7 +2,6 @@ package constants
 
 const LangName = "donkey"
 
-//const ReplPrompt = "\u001b[33m💡 << \u001b[0m"
-const ReplPrompt = ""
+const ReplPrompt = "\u001b[33m💡 >> \u001b[0m"
 
 const ParserErrorPrompt = "🚨 parser errors:\n"
